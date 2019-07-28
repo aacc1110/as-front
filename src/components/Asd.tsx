667784afa11411c60754e8e9700329f976189a57
aacc1110 <tadrow@daum.net>
2019-07-28 13:33:33 +0900

    as-front

---
+++ src/components/Asd.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AsdBlock = styled.div``;
+
+interface AsdProps {}
+
+const Asd: React.FC<AsdProps> = props => {
+  return <AsdBlock>asd</AsdBlock>;
+};
+
+export default Asd;
